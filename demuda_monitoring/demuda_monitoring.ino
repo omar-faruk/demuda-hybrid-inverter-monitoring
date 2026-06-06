342fa4c15cf1f460c0030aa20060f6c37761cea6
Omar <omarfaruk.dc@gmail.com>
2026-06-07 00:33:54 +0600

    data aquisition interval logic changed

--- demuda_monitoring/demuda_monitoring.ino
+++ demuda_monitoring/demuda_monitoring.ino
@@ -7,7 +7,8 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 char mqtt_buffer[1024];
-
+static uint8_t data_interval;
+static unsigned long lastPublish = 0;
 // ---------------- Modbus ----------------
 ModbusMaster node;
 
@@ -22,12 +23,26 @@ void postTransmission() {
   // digitalWrite(RE_DE, LOW);
 }
 
-void mqtt_callback(char *topic, byte *payload, unsigned int length)
-{
+void mqtt_callback(char *topic, byte *payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
 
+  JsonDocument doc;
+
+  // 2. Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, payload, length);
+
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.f_str());
+    return;
+  }
+
+  if (doc.containsKey("data_interval")) {
+    data_interval = (uint16_t)doc["data_interval"];
+    Serial.println("Data Time Updated: " + String(data_interval));
+  }
 }
 // ---------------- Helpers ----------------
 float readReg(uint16_t reg, float scale, bool isSigned = false) {
@@ -54,8 +69,8 @@ void setup() {
   digitalWrite(RE_DE, LOW);
 
   // Modbus init (Serial2 example)
-  Serial2.begin(9600, SERIAL_8N1, 16, 17); // RX=16 TX=17
-  node.begin(1, Serial2); // slave ID = 1
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);  // RX=16 TX=17
+  node.begin(1, Serial2);                   // slave ID = 1
   node.preTransmission(preTransmission);
   node.postTransmission(postTransmission);
 
@@ -63,17 +78,23 @@ void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) delay(500);
 
+  Serial.println("WiFi Connected");
   // MQTT
   client.setBufferSize(1024);
-  client.setServer(mqtt_server, 1883);
-
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(mqtt_callback);
+  data_interval = 30;
+  reconnect();
 }
 
 // ---------------- MQTT reconnect ----------------
 void reconnect() {
+
+  Serial.println("Reconnecting to MQTT Server..");
   while (!client.connected()) {
-    if (client.connect("ESP32_Solar_Node")) {
+    if (client.connect("ESP32_Inverter_Mon", mqtt_user, mqtt_pass)) {
       Serial.println("MQTT connected");
+      client.subscribe(SUB_TOPIC);
     } else {
       Serial.print("MQTT failed, rc=");
       Serial.println(client.state());
@@ -88,66 +109,74 @@ float invert(float v) {
 
 // ---------------- Main loop ----------------
 void loop() {
-  
-  
-  if (WiFi.status() != WL_CONNECTED)
-  {
+
+
+  if (WiFi.status() != WL_CONNECTED) {
     WiFi.reconnect();
   }
-  StaticJsonDocument<1024> doc;
+  client.loop();  // keep MQTT alive
 
-  // ========== AC INPUT ==========
-  doc["ac_in_v"] = readReg(80, 10);
-  doc["ac_in_hz"] = readReg(82, 100);
+  if (millis() - lastPublish > (data_interval * 1000))
+  {
 
-  // ========== AC OUTPUT ==========
-  doc["ac_out_v"] = readReg(88, 10);
-  doc["ac_out_hz"] = readReg(90, 100);
-  doc["ac_out_va"] = readRegRaw(91);
-  doc["ac_out_w"]  = readRegRaw(92);
-  doc["load_pct"]  = readReg(93, 10);
-  doc["inv_out_w"] = readRegRaw(94);
 
-  // ========== BATTERY ==========
-  doc["bat_v"] = readReg(128, 10);
-  doc["bat_a"] = invert(readReg(129, 10, true));
+    StaticJsonDocument<1024> doc;
 
-  // ========== GRID ==========
-  doc["grid_w"] = readRegRaw(149);
+    // ========== AC INPUT ==========
+    doc["ac_in_v"] = readReg(80, 10);
+    doc["ac_in_hz"] = readReg(82, 100);
 
-  // ========== CHARGING ==========
-  doc["charge_v_const"] = readReg(375, 10);
-  doc["charge_v_float"] = readReg(376, 10);
-  doc["pv_charge_limit_a"] = readReg(377, 10);
+    // ========== AC OUTPUT ==========
+    doc["ac_out_v"] = readReg(88, 10);
+    doc["ac_out_hz"] = readReg(90, 100);
+    doc["ac_out_va"] = readRegRaw(91);
+    doc["ac_out_w"] = readRegRaw(92);
+    doc["load_pct"] = readReg(93, 10);
+    doc["inv_out_w"] = readRegRaw(94);
 
-  // ========== PV ==========
-  doc["pv_v"] = readReg(624, 10);
-  doc["pv_a"] = readReg(625, 100, true);
-  doc["pv_w"] = readReg(626,1,true);
-  doc["pv_today_kwh"] = readReg(630, 100);
+    // ========== BATTERY ==========
+    doc["bat_v"] = readReg(128, 10);
+    doc["bat_a"] = invert(readReg(129, 10, true));
 
-  // ========== ENERGY COUNTERS ==========
-  doc["pv_lifetime_kwh"] = readRegRaw(618); // simplified raw view
+    // ========== GRID ==========
+    doc["grid_w"] = readRegRaw(149);
 
-  // Serialize JSON
-  memset(mqtt_buffer,0,1024);
-  size_t n = serializeJson(doc, mqtt_buffer);
+    // ========== CHARGING ==========
+    doc["charge_v_const"] = readReg(375, 10);
+    doc["charge_v_float"] = readReg(376, 10);
+    doc["pv_charge_limit_a"] = readReg(377, 10);
 
-  // Publish MQTT
+    // ========== PV ==========
+    doc["pv_v"] = readReg(624, 10);
+    doc["pv_a"] = readReg(625, 100, true);
+    doc["pv_w"] = readReg(626, 1, true);
+    doc["pv_today_kwh"] = readReg(630, 100);
+
+    // ========== ENERGY COUNTERS ==========
+    doc["pv_lifetime_kwh"] = readRegRaw(618);  // simplified raw view
+
+    // Serialize JSON
+    memset(mqtt_buffer, 0, 1024);
+    size_t n = serializeJson(doc, mqtt_buffer);
+
+
   if (!client.connected())
   {
     reconnect();
   }
-
-  if (client.connected())
-  {
-    client.publish(mqtt_topic, mqtt_buffer);
+    // Publish MQTT
+    if (client.connected())
+    {
+      client.publish(mqtt_topic, mqtt_buffer);
+    } 
+    else
+    {
+      Serial.println("Client Not Connected");
+    }
+    
+    lastPublish = millis();
+    Serial.println(mqtt_buffer);
   }
-  else{
-    Serial.println("Client Not Connected");
-  }
-  
-  Serial.println(mqtt_buffer);
 
-  delay(5000); // 5s update cycle
+  delay(1000);  // 5s update cycle
 }
